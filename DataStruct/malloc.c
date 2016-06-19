aeb6cbae026a59adaf264b6fea96b0bce9763a5e
user <user@Yashs-MacBook-Pro.local>
2016-06-19 15:27:03 +0530

    malloc

---
+++ DataStruct/malloc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+ int i=10,n;
+ char * x;
+
+ x= (char*) malloc(i+1);
+ 
+ for(n=0;n<i;n++) 	x[n]='a'+n;
+ x[i]='\0';
+
+ printf("String is:\n%s\n",x);
+ free(x);
+}
